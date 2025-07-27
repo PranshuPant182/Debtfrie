8e6d838419878679a4f2d48ac4ede6a9354d77b0
PranshuPant182 <gamingm425@gmail.com>
2025-07-27 21:56:59 +0530

    Update db.js

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://pranshupant66:rAb1M7rAp0FM8Tb1@debtfrie.zlhdljv.mongodb.net/?retryWrites=true&w=majority&appName=Debtfrie");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
