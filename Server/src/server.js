f34ce85981b7a54c6f040623279ded0f33e76a4c
PranshuPant182 <gamingm425@gmail.com>
2026-03-25 13:30:22 +0530

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -2,12 +2,10 @@ require('dotenv').config({
   path: require('path').resolve(__dirname, '../.env')
 });
 
-const connectDB = require('./config/db'); // MongoDB connection
-const app = require('./app'); // Express app
+const connectDB = require('./config/db');
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-
-// Add this line 👇 before starting the server
 console.log("Trying to start server on port:", PORT);
 
 // Connect to MongoDB and start the server
@@ -16,5 +14,5 @@ connectDB().then(() => {
     console.log(`✅ Server running at http://localhost:${PORT}`);
   });
 }).catch((err) => {
-  console.error('❌ Failed to start server:', err.message);
+  console.error('Failed to start server:', err.message);
 });
